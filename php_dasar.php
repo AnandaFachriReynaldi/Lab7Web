2340ce05e502e50ebe960de65cefcfac250de3e3
Ananda Fachri Reynaldi <afrynld@gmail.com>
2022-11-18 19:17:04 +0700

    Menambahkan File php_dasar.php

---
+++ php_dasar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Dasar</title>
+    </head>
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+    echo "Hello World";
+    ?>
+
+    <h2>Menggunakan Variable</h2>
+    <?php
+    $nim = "312110248";
+    $nama = 'Ananda Fachri Reynaldi';
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama";
+    ?>
+</body>
+</html
